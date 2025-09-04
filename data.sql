62ca1dc160744d94d5ab8097513d124013d9dee9
ahmed <a.t.elgammal@gmail.com>
2025-09-04 15:03:04 +0400

    drop the table and create new one with propper columns

--- data.sql
+++ data.sql
@@ -1,2 +1,14 @@
-ALTER TABLE used_cars_general RENAME
-TO used_cars_staging;
+CREATE TABLE UAE_used_cars_staging(
+    brand TEXT,
+    model TEXT,
+    p_year TEXT,
+    price TEXT,
+    milage TEXT,
+    body_type TEXT,
+    cylinders TEXT,
+    transmission_type TEXT,
+    fuel_type TEXT,
+    color TEXT,
+    emirate TEXT,
+    description TEXT
+);
