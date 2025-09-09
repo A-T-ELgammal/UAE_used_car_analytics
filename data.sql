7421f0f35fdec9e2ed2cd112a6fe77fd9a6cb14b
ahmed <a.t.elgammal@gmail.com>
2025-09-09 15:59:34 +0400

    export the cleaned data into csv file

--- data.sql
+++ data.sql
@@ -1,16 +1,1 @@
-INSERT INTO uae_used_cars_cleaned 
-SELECT 
-    brand,
-    model,
-    p_year::INT,
-    price::INT,
-    milage::INT,
-    body_type::VARCHAR(25),
-    cylinders::INT,
-    transmission_type::VARCHAR(10),
-    fuel_type::VARCHAR(10),
-    color,
-    emirate,
-    description
-FROM uae_used_cars_staging
-
+SELECT * FROM uae_used_cars_cleaned
