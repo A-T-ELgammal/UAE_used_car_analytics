779a5603d6a3835bd8d77ed1d1f9a56c869d9a8c
ahmed <a.t.elgammal@gmail.com>
2025-09-09 14:04:40 +0400

    check for null or unkown values in all records

--- data.sql
+++ data.sql
@@ -1,3 +1,24 @@
-SELECT emirate , COUNT(emirate)
+SELECT brand , COUNT(brand)
+        
 FROM uae_used_cars_staging
-GROUP BY emirate;
+GROUP BY brand;
+
+SELECT model , COUNT(model)
+        
+FROM uae_used_cars_staging
+GROUP BY model;
+
+SELECT body_type , COUNT(body_type)
+        
+FROM uae_used_cars_staging
+GROUP BY body_type;
+
+SELECT fuel_type , COUNT(fuel_type)
+        
+FROM uae_used_cars_staging
+GROUP BY fuel_type;
+
+SELECT color , COUNT(color)
+        
+FROM uae_used_cars_staging
+GROUP BY color;
