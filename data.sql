ee295ec50b1a512f898668dd9144b00ed414af4e
ahmed <a.t.elgammal@gmail.com>
2025-09-09 15:02:11 +0400

    unify the all values of transmission_type to be one word without spaces

--- data.sql
+++ data.sql
@@ -1,2 +1,7 @@
-DELETE FROM uae_used_cars_staging
-WHERE model LIKE '%other%';
+UPDATE uae_used_cars_staging
+SET transmission_type = CASE
+    WHEN transmission_type = 'Automatic Transmission' THEN 'Automatic'
+    WHEN transmission_type = 'Manual Transmission' THEN 'Manual'
+END
+
+    WHERE transmission_type IN ('Automatic Transmission', 'Manual Transmission')
