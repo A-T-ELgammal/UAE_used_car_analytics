273afe6cc930eaa599991821f2eb9c2ebdf1f70d
ahmed <a.t.elgammal@gmail.com>
2025-09-09 15:05:00 +0400

    validate last update of transmission_type

--- data.sql
+++ data.sql
@@ -1,7 +1,1 @@
-UPDATE uae_used_cars_staging
-SET transmission_type = CASE
-    WHEN transmission_type = 'Automatic Transmission' THEN 'Automatic'
-    WHEN transmission_type = 'Manual Transmission' THEN 'Manual'
-END
-
-    WHERE transmission_type IN ('Automatic Transmission', 'Manual Transmission')
+SELECT DISTINCT transmission_type FROM uae_used_cars_staging
