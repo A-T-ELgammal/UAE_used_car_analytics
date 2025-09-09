f6823484705ab0842af831496279dfd9dbbf26f8
ahmed <a.t.elgammal@gmail.com>
2025-09-09 12:03:16 +0400

    update fujeirah emirate records

--- data.sql
+++ data.sql
@@ -1,8 +1,3 @@
 UPDATE uae_used_cars_staging
-SET emirate = CASE
-    WHEN emirate = ' Abu Dhabi' THEN 'Abu Dhabi'
-    WHEN emirate = ' Fujeirah' THEN ' Fujeirah'
-ELSE emirate
-END
-
-WHERE emirate IN (' Abu Dhabi', ' Fujeirah')
+SET emirate = 'Fujeirah'
+WHERE emirate = ' Fujeirah';
