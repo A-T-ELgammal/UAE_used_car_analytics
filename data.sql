47fe2e6cb8b1f5e2ea02b9ee535272ec73d87f1e
ahmed <a.t.elgammal@gmail.com>
2025-09-09 14:48:54 +0400

    fill cylinders for all electric cars

--- data.sql
+++ data.sql
@@ -1,18 +1,4 @@
--- select p_year, price, milage, cylinders
--- FROm uae_used_cars_staging
--- WHERE p_year = '[A-Za-z]' OR
---         price = '[A-Za-z]' OR 
---         milage = '[A-Za-z]' OR
---         cylinders = '[A-Za-z]'
---         OR ;
 
-SELECT *
-FROM uae_used_cars_staging
-WHERE p_year = '[A-Za-z]' 
-    OR price = '[A-Za-z]'
-    OR milage = '[A-Za-z]'
-    OR cylinders = '[A-Za-z]'
-    OR p_year IS NULL
-    OR price IS NULL
-    OR milage IS NULL
-    OR cylinders IS NULL;
+UPDATE uae_used_cars_staging
+SET cylinders = '0'
+WHERE fuel_type = 'Electric';
