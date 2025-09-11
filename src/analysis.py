104639cab85e384f42b40bf2b36dafcda2b91768
ahmed <a.t.elgammal@gmail.com>
2025-09-11 15:53:41 +0400

    connect postgresql database to sqlalchemy & tested successfully

--- src/analysis.py
+++ src/analysis.py
@@ -2,10 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import pandas as pd
-
+import sqlalchemy 
 
 used_cars = pd.read_csv('/home/ahmed/projects/data_analysis/projects/data/data_cleaned.csv',delimiter= ',')
 
-print(used_cars.info())
-print('--------------------------------')
-print(used_cars.isnull())
+postgresql_user = 'postgres'
+postgresql_password = '123456'
+host = 'localhost'
+port = '5432'
+database = 'UAE_used_cars'
+
+connection_request = f'postgresql+psycopg2://{postgresql_user}:{postgresql_password}@{host}:{port}/{database}'
+engine = sqlalchemy.create_engine(connection_request)
+
+try:
+    with engine.connect() as connection:
+        print('Successfully connected to the PostgreSQL database')
+except exption as ex:
+    print(f'failed to connect to postgreql due to this {ex}')
