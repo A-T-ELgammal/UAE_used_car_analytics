32cd85432532779d52970d7421e198a083e0f71a
ahmed <a.t.elgammal@gmail.com>
2025-09-11 19:03:19 +0400

    create emirates rankning sql query & pass it into data frame & plot it as barplot colored & save into output images

--- src/analysis.py
+++ src/analysis.py
@@ -20,3 +20,16 @@ try:
         print('Successfully connected to the PostgreSQL database')
 except Exception as ex:
     print(f'failed to connect to postgreql due to this {ex}')
+
+emirates_ranking_query = " SELECT emirate, COUNT(emirate) AS number_of_cars FROM uae_used_cars_cleaned GROUP BY emirate;"
+
+emirares_df = pd.read_sql(emirates_ranking_query, engine)
+emirares_df = emirares_df.sort_values('number_of_cars', ascending= False)
+palette = sns.color_palette("Set2", len(emirares_df))
+
+sns.barplot(data= emirares_df, x= 'number_of_cars', y= 'emirate', palette = palette)
+plt.title('emirates_ranking_of_used_cars')
+plt.xlabel('used_cars_available')
+plt.ylabel('Emirate')
+plt.savefig('/home/ahmed/projects/data_analysis/projects/ouput/plotting_images/emirates_ranking.png', dpi = 300, bbox_inches = 'tight')
+plt.show()
