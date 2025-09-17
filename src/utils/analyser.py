1e08863561435f6d283d5a9f472ceefe62f22913
ahmed <a.t.elgammal@gmail.com>
2025-09-17 16:08:59 +0400

    sort the emirates & sum the emirates below 5% of cars to be 'others' & visualize it with pieChart

--- src/utils/analyser.py
+++ src/utils/analyser.py
@@ -18,11 +18,19 @@ def main():
     # visualizer = visualizer()
 
     sql_connection.postgres_connect('postgres','123456', 'localhost', '5432', 'UAE_used_cars')
+    
     emirares_df = sql_connection.query_df(emirates_ranking_query)
     emirares_df = emirares_df.sort_values('number_of_cars', ascending= False)
+   
+    print('--------------------')
+    total_cars = emirares_df['number_of_cars'].sum()
+    for record in range(emirares_df.shape[0]):
+        if(emirares_df.iloc[record][1] * 100 / total_cars <= 5):
+            emirares_df.iloc[record, 0] = "other"
+    emirares_df = emirares_df.groupby('emirate', as_index= False)['number_of_cars'].sum()
+    print('---------------------')
+   
+    print(emirares_df)
     visualizer.pieChart_plot(emirares_df['emirate'], emirares_df['number_of_cars'], 'emirates_ranknig_pie')
-    # print(emirares_df)    
-
-
 if __name__ == "__main__":
     main()
--- src/utils/visualizer.py
+++ src/utils/visualizer.py
@@ -6,7 +6,8 @@ import seaborn as sns
     
 def pieChart_plot(labels: pd.DataFrame, values: pd.DataFrame, plot_name: str):
     colors = sns.color_palette('bright6')
-    plt.pie(values, labels=labels, colors= colors, autopct='%.2f%%', pctdistance= 0.8)
+    plt.pie(values, colors= colors, autopct='%.2f%%', pctdistance= 0.8)
     plot_path = f'/home/ahmed/projects/data_analysis/projects/ouput/plotting_images/{plot_name}'
+    plt.legend(labels,)
     plt.savefig(plot_path)
     plt.show()
