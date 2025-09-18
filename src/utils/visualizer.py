844121593293f09c2f5bf62f771436dad241d8c8
ahmed <a.t.elgammal@gmail.com>
2025-09-18 15:40:48 +0400

    visualize bar_plot function

--- src/utils/visualizer.py
+++ src/utils/visualizer.py
@@ -11,3 +11,11 @@ def pieChart_plot(labels: pd.DataFrame, values: pd.DataFrame, plot_name: str):
     plt.legend(labels)
     plt.savefig(plot_path)
     plt.show()
+
+def barChart_plot(values: pd.DataFrame, labels: pd.DataFrame, x_name: str, y_name: str, title: str):
+    sns.barplot(x= values, y= labels)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.title(title)
+    plt.savefig(f'/home/ahmed/projects/data_analysis/projects/ouput/plotting_images/{title}')
+    plt.show()
